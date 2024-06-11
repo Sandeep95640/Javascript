4168c4b5762001ae8d507f2b257feb53b1680423
Sandeep95640 <kumarsandeep95640@gmail.com>
2024-06-11 19:36:33 +0530

    JS

---
+++ scripts/12-todo-list.js
@@ -0,0 +1,62 @@
+//default todolist items of arrayObject
+const todoList = [{
+  name: 'make dinner',
+  dueDate: '2022-12-22'
+}, {
+  name: 'wash dishes',
+  dueDate: '2022-12-22'
+}];
+
+renderTodoList();//to show the default previous arrayObject
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  todoList.forEach((todoObject, index) => {
+
+    const { name, dueDate } = todoObject;
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button class="delete-todo-button js-delete-todo-button">Delete</button> 
+    `;
+    todoListHTML += html;
+  });
+
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
+
+  document.querySelectorAll('.js-delete-todo-button')
+  .forEach((deleteButton, index) => {
+    deleteButton.addEventListener('click', () => {
+      todoList.splice(index, 1);
+        renderTodoList();
+    })
+  });
+}
+
+document.querySelector('.js-add-todo-button').addEventListener('click', () => {
+  addTodo();
+});
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  if (name === '' || dueDate === '') {
+    alert('Please enter both a task name and a due date.');
+  } else {
+    todoList.push({
+      name,
+      dueDate
+    });
+
+    inputElement.value = '';
+    dateInputElement.value = '';
+
+    renderTodoList();
+  }
+}
